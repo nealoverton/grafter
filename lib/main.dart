688c1bbcce764f51128a5cea5f7b3405046f770a
nealoverton <nealjoverton@gmail.com>
2022-02-21 17:46:53 +0200

    remove unused imports

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,6 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:grafter/models/app_user.dart';
-import 'package:grafter/screens/authentication_wrapper.dart';
-import 'package:grafter/screens/home.dart';
 import 'package:grafter/screens/logged_in_wrapper.dart';
 import 'package:grafter/services/auth.dart';
 import 'package:provider/provider.dart';
@@ -16,7 +14,6 @@ void main() async {
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return StreamProvider<AppUser?>(
