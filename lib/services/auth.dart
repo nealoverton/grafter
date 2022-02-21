fbe9c8406053c6c460abb7694c619a26985d8438
nealoverton <nealjoverton@gmail.com>
2022-02-21 14:06:07 +0200

    register on firebase with email and password

---
+++ lib/services/auth.dart
@@ -0,0 +1,24 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:graftr/models/app_user.dart';
+
+class AuthService {
+  final FirebaseAuth _auth = FirebaseAuth.instance;
+
+  Future registerWithEmailAndPassword(String email, String password) async {
+    try {
+      UserCredential result = await _auth.createUserWithEmailAndPassword(
+          email: email.trim(), password: password.trim());
+
+      User? user = result.user;
+
+      return _userFromFirebaseUser(user);
+    } catch (e) {
+      print(e);
+      return null;
+    }
+  }
+
+  AppUser? _userFromFirebaseUser(User? user) {
+    return user != null ? AppUser(uid: user.uid) : null;
+  }
+}
