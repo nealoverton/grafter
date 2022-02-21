29a3e42ba8b24d235b2572eedb243b8177a6e19f
nealoverton <nealjoverton@gmail.com>
2022-02-21 15:30:03 +0200

    refactor app name

--- lib/screens/authentication_wrapper.dart
+++ lib/screens/authentication_wrapper.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:graftr/screens/login.dart';
-import 'package:graftr/screens/register.dart';
+import 'package:grafter/screens/login.dart';
+import 'package:grafter/screens/register.dart';
 
 class Authenticate extends StatefulWidget {
   const Authenticate({Key? key}) : super(key: key);
