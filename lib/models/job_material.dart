73600ea6163bd5c0a80182a1ee5d1018e8ed6aed
nealoverton <nealjoverton@gmail.com>
2022-02-22 15:20:02 +0200

    require quantity for JobMaterial

--- lib/models/job_material.dart
+++ lib/models/job_material.dart
@@ -1,6 +1,8 @@
 class JobMaterial {
   final String name;
   final double price;
+  final int quantity;
 
-  JobMaterial({required this.name, required this.price});
+  JobMaterial(
+      {required this.name, required this.price, required this.quantity});
 }
