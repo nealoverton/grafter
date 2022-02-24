92e5fde8713ea6bb3d7a840232e3a1e80a988f67
Jordan Richmond <richmond42@ymail.com>
2022-02-24 14:34:57 +0000

    added list screen

---
+++ lib/screens/list_screen.dart
@@ -0,0 +1,76 @@
+// ignore_for_file: prefer_const_constructors
+
+import 'package:flutter/material.dart';
+import 'package:grafter/models/Note.dart';
+import 'package:grafter/models/NotesOperation.dart';
+import 'package:grafter/screens/add_screen.dart';
+import 'package:provider/provider.dart';
+
+class ListScreen extends StatelessWidget {
+  const ListScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.lightBlue,
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          Navigator.push(
+              context, MaterialPageRoute(builder: (context) => AddScreen()));
+        },
+        child: Icon(
+          Icons.add,
+          size: 30,
+          color: Colors.lightBlue,
+        ),
+        backgroundColor: Colors.white,
+      ),
+      appBar: AppBar(
+        title: const Text('Job List'),
+        centerTitle: true,
+        elevation: 0,
+        backgroundColor: Colors.transparent,
+      ),
+      body: Consumer<NotesOperation>(
+        builder: (context, NotesOperation data, child) {
+          return ListView.builder(
+            itemCount: data.getNotes.length,
+            itemBuilder: (context, index) {
+              return NotesCard(data.getNotes[index]);
+            },
+          );
+        },
+      ),
+    );
+  }
+}
+
+class NotesCard extends StatelessWidget {
+  final Note note;
+
+  NotesCard(this.note);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: EdgeInsets.all(15),
+      padding: EdgeInsets.all(15),
+      height: 150,
+      decoration: BoxDecoration(
+          color: Colors.white, borderRadius: BorderRadius.circular(15)),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Text(
+            note.title,
+            style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+          ),
+          SizedBox(
+            height: 5,
+          ),
+          Text(note.description, style: TextStyle(fontSize: 17))
+        ],
+      ),
+    );
+  }
+}
